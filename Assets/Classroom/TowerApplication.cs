63f5251056265f934c9398e4e15924cb1beddbe8
Max Jonsson <jag@maxjonsson.se>
2018-11-22 17:21:37 +0100

    TDD style - first failing end-to-end test

---
+++ Assets/Classroom/TowerApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerApplication : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+  public void pickUp(int indexOfTower)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void putDown(int indexOfTower)
+  {
+    throw new NotImplementedException();
+  }
+
+  public GameBoardState getCurrentSceneState()
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ Assets/Editor/ApplicationEndToEndTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ApplicationEndToEndTest
+{
+  [Test]
+  public void ApplicationEndToEndTestSimplePasses()
+  {
+    // Use the Assert class to test conditions.
+  }
+
+  // A UnityTest behaves like a coroutine in PlayMode
+  // and allows you to yield null to skip a frame in EditMode
+  [UnityTest]
+  public IEnumerator ApplicationEndToEndTestWithEnumeratorPasses()
+  {
+    // Use the Assert class to test conditions.
+    // yield to skip a frame
+    yield return null;
+  }
+
+  [Test]
+  public void towerPieceMoved_afterUserInput() {
+    TowerApplication application = new TowerApplication();
+    GameBoardState startState = application.getCurrentSceneState();
+
+    application.pickUp(0);
+    application.putDown(1);
+
+    GameBoardState state = application.getCurrentSceneState();
+    Assert.That(state != startState);
+  }
+
+}
